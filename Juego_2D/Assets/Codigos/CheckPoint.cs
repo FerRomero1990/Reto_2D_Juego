e9dc1773b7395ce513da90612147032e9e7208cb
Fernanda Romero <aidafernanda1990@hotmail.com>
2023-11-02 14:51:29 -0500

    Nivel 3 Finalizado
    Se finaliza el nivel 3 y se adicionan: vidas, cronometro y control de recolección.

--- Juego_2D/Assets/Codigos/CheckPoint.cs
+++ Juego_2D/Assets/Codigos/CheckPoint.cs
@@ -7,7 +7,13 @@ public class CheckPoint : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D col)
     {
+        //Debug.Log("Movimiento personaje");
         if (col.gameObject.CompareTag("Player"))
+        {
             col.gameObject.GetComponent<MovimientoPersonaje>().posicionInicio = transform.position;
+            print("Colisono");
+            
+        }
+            
     }
 }
